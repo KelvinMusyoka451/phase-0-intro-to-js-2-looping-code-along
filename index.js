23c37078a7c516e158d48b2bc5e8a420edf6ff77
kelvin <kelvin.musyoka@student.moringaschool.com>
2023-12-17 15:35:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+const names = ["Kelvin", "Philip", "Naomi", "Erick"];
+const eventName = "swimmingGala";
+
+function writeCards(names, eventName){
+  const messages = [];
+  for(let i = 0; i < names.length; i++){
+    const congrats = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    messages.push(congrats);
+  }
+  return messages;
+}
+const finalMessage = writeCards(names, eventName);
+console.log(finalMessage);
+
+
+
+const number = 20;
+function countDown(number){
+  let i = number;
+  while (i >= 0){
+    console.log(i);
+    i--;
+  }
+  return number;
+}
